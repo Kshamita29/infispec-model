3767abcc2bcc77647a69a1b065a6b1bdf3f69c24
Kshamita Kiran Baviskar <kshamita2920@gmail.com>
2023-09-15 21:10:46 +0530

    a

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -20,7 +20,7 @@ import ContactUs from '../pages/ContactUs';
 const Routers = () => {
   return (
       <Routes> 
-          <Route path="/infispec-model" element={<Home/>} />
+          <Route exact path="/infispec-model" element={<Home/>} />
           <Route path="/services" element={<AllServices />} />
           <Route path='/about' element={<About />}/>
           <Route path='/team' element={<TeamMembers />}/>
