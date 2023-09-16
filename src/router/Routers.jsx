0ba987cf0563e3c71fed7b8227016465065a4c58
Kshamita Kiran Baviskar <kshamita2920@gmail.com>
2023-09-16 15:15:33 +0530

    changes pushed

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -20,19 +20,19 @@ const path = process.env.REACT_APP_FOR_PATH;
 const Routers = () => {
   return (
       <Routes> 
-          <Route path={`${path}/`} element={<Home/>} />
-          <Route path={`${path}/services`}element={<AllServices />} />
-          <Route path={`${path}/about`} element={<About />}/>
-          <Route path={`${path}/team`} element={<TeamMembers />}/>
-          <Route path={`${path}/projects`} element={<Projects />}/>
-          <Route path={`${path}/infinite-insights`} element={<InfiniteInsights />}/>
-          <Route path={`${path}/e-profile`} element={<Profile />}/>
-          <Route path={`${path}/tax`} element={<TaxProConnect />}/>
-          <Route path={`${path}/stay`} element={<StayHub />}/>
-          <Route path={`${path}/why-choose-us`} element={<WhyChooseUs />}/>
-          <Route path={`${path}/v-n-m`} element={<VnM />}/>
-          <Route path={`${path}/core-values`} element={<CoreValues/>}/>
-          <Route path={`${path}/contact-us`} element={<ContactUs/>}/>
+          <Route path={`${path} + "/"`} element={<Home/>} />
+          <Route path={`${path} + "/services"`}element={<AllServices />} />
+          <Route path={`${path} + "/about"`} element={<About />}/>
+          <Route path={`${path} + "/team"`} element={<TeamMembers />}/>
+          <Route path={`${path} + "/projects"`} element={<Projects />}/>
+          <Route path={`${path} + "/infinite-insights"`} element={<InfiniteInsights />}/>
+          <Route path={`${path} + "/e-profile"`} element={<Profile />}/>
+          <Route path={`${path} + "/tax"`} element={<TaxProConnect />}/>
+          <Route path={`${path} + "/stay"`} element={<StayHub />}/>
+          <Route path={`${path} + "/why-choose-us"`} element={<WhyChooseUs />}/>
+          <Route path={`${path} + "/v-n-m"`} element={<VnM />}/>
+          <Route path={`${path} + "/core-values"`} element={<CoreValues/>}/>
+          <Route path={`${path} + "/contact-us"`} element={<ContactUs/>}/>
       </Routes>
     
   );
