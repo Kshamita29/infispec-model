88cd9e5bcda1aacf1d8f76c58df7379ff29613df
Kshamita Kiran Baviskar <kshamita2920@gmail.com>
2023-09-16 14:51:31 +0530

    changes pushed

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter baseline="/">
+    <HashRouter baseline="/infispec-model/">
       <App />
     </HashRouter>
   </React.StrictMode>
